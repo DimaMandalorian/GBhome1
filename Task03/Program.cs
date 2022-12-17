e61804f92af72dc919578919ab59fdb315eb6699
Dmitrij <accordsds@gmail.com>
2022-12-17 05:33:21 -0800

    Внесли изменения else

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -7,27 +7,30 @@ Write("Введите число от 1 до 7: ");
 int a = int.Parse(ReadLine());
 
 if (a == 1)
-{
+    {
     WriteLine("понедельник");
+    }
+        else if (a == 2)
+        {
+        WriteLine("Вторник");
+        }
+            else if (a == 3)
+            {
+            WriteLine("среда");
+            }
+                else if (a == 4)
+                {
+                WriteLine("четверг");
+                }
+                    else if (a == 5)
+                    {
+                    WriteLine("пятница");
+                    }
+                        else if (a == 6)
+                        {
+                        WriteLine("суббота");
+                        }
+                            else if (a == 7) { WriteLine("воскресение"); }
+else {
+    WriteLine("Введено не верное число");
 }
-else if (a == 2)
-{
-    WriteLine("Вторник");
-}
-else if (a == 3)
-{
-    WriteLine("среда");
-}
-else if (a == 4)
-{
-    WriteLine("четверг");
-}
-else if (a == 5)
-{
-    WriteLine("пятница");
-}
-else if (a == 6)
-{
-    WriteLine("суббота");
-}
-else if (a == 7) { WriteLine("воскресение"); }
