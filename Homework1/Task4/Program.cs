8b4515c50654639bb1472ca9b63bd0d76276d59d
Dmitrij <accordsds@gmail.com>
2022-12-18 01:55:00 -0800

    Max из  трех чисел

--- Homework1/Task4/Program.cs
+++ Homework1/Task4/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("Максимальное из трех чисел");
+Write("Введите число a: ");
+int a = int.Parse(ReadLine()!);
+Write("Введите число b: ");
+int b = int.Parse(ReadLine()!);
+Write("Введите число c: ");
+int c = int.Parse(ReadLine()!);
+
+if (a>b && a>c)
+{WriteLine(a);}
+else
+    if (b>c)
+    {WriteLine(b);}
+    else
+        {WriteLine(c);}
