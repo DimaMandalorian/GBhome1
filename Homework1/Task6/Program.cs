795b7e7d56f3e6ee947f5e8a5783a9746542ae5f
Dmitrij <accordsds@gmail.com>
2022-12-18 00:39:40 -0800

    50/50 Я и Гугл

---
+++ Homework1/Task6/Program.cs
@@ -0,0 +1,9 @@
+﻿using static System.Console;
+
+Console.Write("Введитe число ");
+int n = int.Parse(Console.ReadLine()!);
+double n_2 = n % 2;
+if (n_2==0) 
+    WriteLine("да");
+else
+WriteLine("нет");
