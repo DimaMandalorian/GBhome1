87a7dad6db1be25c0148dd1d390d2e7a67fd2990
Dmitrij <accordsds@gmail.com>
2022-12-18 17:36:22 -0800

    На входе число, на выходе етные числа

---
+++ Homework1/Task8/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+
+Write("Введите число: ");
+int a = int.Parse(ReadLine()!);
+int n = 1; 
+
+while (n <= a ) 
+{
+    if (n % 2 == 0)
+    Write($"{n} ");
+    n++;   
+}
