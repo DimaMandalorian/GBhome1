721e42064325154fee807acb13e49a35b6457cc6
Dmitrij <accordsds@gmail.com>
2022-12-21 03:50:48 -0800

    Ввод два числа,Является ли одно квадратом другого

---
+++ Seminar2/Task16/Program.cs
@@ -0,0 +1,10 @@
+﻿using static System.Console;
+
+WriteLine("Введите первое число a: ");
+int a = Convert.ToInt32(ReadLine());
+WriteLine("Введите первое число b: ");
+int b = Convert.ToInt32(ReadLine());
+
+//if (a*a==b || b*b==a)
+
+WriteLine(a*a==b || b*b==a? "является": "не является");
