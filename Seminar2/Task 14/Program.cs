da2888190c07e8bb9731070b51b72281f72e321a
Dmitrij <accordsds@gmail.com>
2022-12-21 03:32:50 -0800

    Вводим число,кратно ли сразу двум числам 7 и 23

---
+++ Seminar2/Task 14/Program.cs
@@ -0,0 +1,6 @@
+﻿using static System.Console;
+
+WriteLine("Введите первое число a: ");
+int number1 = Convert.ToInt32(ReadLine());
+
+WriteLine(number1 % 7 == 0 && number1 % 23 == 0? "кратно": "некратно");
