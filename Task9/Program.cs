b5915ad7cdd3a9a1759101d54e2c79c27efaceee
Dmitrij <accordsds@gmail.com>
2022-12-21 00:47:49 -0800

    Тернарный оператор

---
+++ Task9/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+Clear();
+int namber=new Random().Next(10,100);
+WriteLine(namber);
+WriteLine(namber%2==0?"Even":"add");
+WriteLine(namber/10>namber%10?namber/10:namber%10);
+
+//if(a1>a2);
+//{
+//   WriteLine(a1);
+//}
+//else
+//{
+//   WriteLine(a2);
+//}
