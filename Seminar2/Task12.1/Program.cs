0f463081dd41bb2fd03d3ec1ef3494ce927c9d00
Dmitrij <accordsds@gmail.com>
2022-12-21 02:41:14 -0800

    Вводим 2 числа, проверяем >,<,=

---
+++ Seminar2/Task12.1/Program.cs
@@ -0,0 +1,8 @@
+﻿using static System.Console;
+
+WriteLine("Введите первое число a: ");
+int a = Convert.ToInt32(ReadLine());
+WriteLine("Введите первое число b: ");
+int b = Convert.ToInt32(ReadLine());
+
+WriteLine(a==b? "=": a>b? ">": "<");
