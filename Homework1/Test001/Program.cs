44b7d5c09e4ceadb12efe44007e07956d631b739
Dmitrij <accordsds@gmail.com>
2022-12-22 01:35:05 -0800

    Цифра дня недели,является ли выходным

---
+++ Homework1/Test001/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+
+Write("Введите число: ");
+int a = int.Parse(ReadLine()!);
+
+
+if(a<99)
+{
+     WriteLine("третьей цифры нет");
+}
+else
+{
+WriteLine(a%10);
+}
---
+++ Homework2/Task15/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+
+Write("Введите число от 1 до 7: ");
+int a = int.Parse(ReadLine());
+
+if (a == 6)
+    {
+    WriteLine("да");
+    }
+        else if (a == 7)
+        {
+        WriteLine("да");
+        }
+            else 
+            {
+            WriteLine("нет");
+            }
