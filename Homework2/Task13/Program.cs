7951ae6607bba766c7823ad42f22cd795cb6dee3
Dmitrij <accordsds@gmail.com>
2022-12-23 00:33:42 -0800

    Из трех данных чисел найти третью цифру,либо ее нет

---
+++ Homework2/Task13/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console; 
+ 
+Write("Введите число: "); 
+int a = int.Parse(ReadLine()!); 
+ 
+if(a<99) 
+{ 
+    WriteLine("третьей цифры нет"); 
+} 
+else if(a>999) 
+{ 
+    WriteLine((a/100)%10); 
+}
+else 
+{ 
+    WriteLine(a%10); 
+}
