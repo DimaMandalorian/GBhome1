61d45989892fb170b7d7051e5dec0727bab4800e
Dmitrij <accordsds@gmail.com>
2022-12-24 00:44:46 -0800

    Вести числа кооординат X и Y, найти четверть

---
+++ Seminar2/Task18.1/Program.cs
@@ -0,0 +1,39 @@
+﻿using static System.Console;
+Clear();
+
+Write("Введите число четверти: ");
+int x=int.Parse(ReadLine()!);
+Write("Введите число четверти: ");
+int y=int.Parse(ReadLine()!);
+
+WriteLine("");
+WriteLine("Четверти:");
+WriteLine(" II I ");
+WriteLine("III IV");
+WriteLine("");
+
+if (x==0 || y==0)
+{
+    WriteLine("Error");
+    return;
+}
+if (x>0 && y>0)
+{
+    WriteLine("I четверть");
+    return;
+}
+if (x<0 && y>0)
+{
+    WriteLine("II четверть");
+    return;
+}
+if (x<0 && y<0)
+{
+    WriteLine("III четверть");
+    return;
+}
+if (x>0 && y<0)
+{
+    WriteLine("IV четверть");
+    return;
+}
