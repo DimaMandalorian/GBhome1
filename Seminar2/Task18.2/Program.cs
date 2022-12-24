252564d2a6b50bd925a6063e490a5fe4fc480a96
Dmitrij <accordsds@gmail.com>
2022-12-24 02:27:27 -0800

    На входе число деталей,на выходе сколько сделал каждый из 3х

---
+++ Seminar2/Task18.2/Program.cs
@@ -0,0 +1,11 @@
+﻿using static System.Console;
+Clear();
+
+Write("Введите количество деталей за смену: ");
+int quantity=int.Parse(ReadLine()!);
+int result=quantity%6;
+
+if (result==0)
+WriteLine($"Младшие токари сделали по {quantity/6} детадей, Старший {quantity/6*4} деталей");
+
+else    WriteLine("старший токарь ошибся");
