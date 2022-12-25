70d11d361110750a12c3a103278002882f742740
Dmitrij <accordsds@gmail.com>
2022-12-25 01:24:28 -0800

    Нахождение длинны по трем точкам координат

---
+++ Homework3/Task21/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает н
+// а вход координаты двух точек
+// и находит расстояние между ними в 3D пространстве.
+
+using static System.Console;
+Clear();
+
+Write("Точка A -> Введите координату x: ");
+int.TryParse(ReadLine(), out int ax);
+Write("Точка A -> Введите координату y: ");
+int.TryParse(ReadLine(), out int ay);
+Write("Точка A -> Введите координату z: ");
+int.TryParse(ReadLine(), out int az);
+Write("Точка B -> Введите координату x: ");
+int.TryParse(ReadLine(), out int bx);
+Write("Точка B -> Введите координату y: ");
+int.TryParse(ReadLine(), out int by);
+Write("Точка B -> Введите координату z: ");
+int.TryParse(ReadLine(), out int bz);
+
+double length = Math.Sqrt(Math.Pow((ax-bx),2)+Math.Pow((ay-by),2)+Math.Pow((az-bz),2));
+WriteLine(length);
