7fbeb0ee3a394e8398a787a15d265ed3c768bffa
Dmitrij <accordsds@gmail.com>
2022-12-25 00:05:46 -0800

    Нф входе две координаты точек,найти расстоние

---
+++ Seminar2/Task20/Program.cs
@@ -0,0 +1,18 @@
+﻿//  Напишите программу, которая принамает на вход координаты точек
+// и находит расстояние между ними в 2D пространстве.
+
+
+using static System.Console;
+Clear();
+
+Write("Точка A -> Введите координату x: ");
+int.TryParse(ReadLine(), out int ax);
+Write("Точка A -> Введите координату y: ");
+int.TryParse(ReadLine(), out int ay);
+Write("Точка B -> Введите координату x: ");
+int.TryParse(ReadLine(), out int bx);
+Write("Точка B -> Введите координату y: ");
+int.TryParse(ReadLine(), out int by);
+
+double length = Math.Sqrt(Math.Pow((ax-bx),2)+Math.Pow((ay-by),2));
+WriteLine(length);
