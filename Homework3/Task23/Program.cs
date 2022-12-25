5624de2f21c29c1b5473f94c3fdbef5e3511021a
Dmitrij <accordsds@gmail.com>
2022-12-25 01:38:15 -0800

    Вводим число,получаем куб всех чисел от 1 до N

---
+++ Homework3/Task23/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+using static System.Console;
+Clear();
+int i = 1;
+
+Write("Введите число для вывода таблицы кубов от 1 до числа: ");
+int.TryParse(ReadLine(),out int n);
+
+// for (i=1; i<n+1; i++)
+// {
+//     WriteLine($"Квадрат числа {i} = {Math.Pow(i,2)}");
+// }
+
+while (i <n+1)
+{
+    WriteLine($"Квадрат числа {i} = {Math.Pow(i,3)}");
+    i++;
+}
