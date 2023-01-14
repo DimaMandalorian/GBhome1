5d79aa5cce4e80eac8de6b552efe982bbc6026f8
Dmitrij <accordsds@gmail.com>
2023-01-14 03:47:22 -0800

    Задача на вход число, на выходе количество цифр

--- Seminar2.1/Task24.1/Program.cs
+++ Seminar2.1/Task24.1/Program.cs
@@ -5,20 +5,23 @@
 using static System.Console;
 Clear();
 
-Write("Input number: " );            
-int.TryParse(ReadLine(), out int number);
-WriteLine($"Количество цифр в числе {number} -> {NumCount(number)}");
+Write("Input number: " );      // Вводим число      
+int.TryParse(ReadLine(), out int number); // Проверяем что оно может быть числом 
+                        // Если это число]]]]]]]]6 то присваеваем его к переменной number
+
+WriteLine($"Количество цифр в числе {number} -> {NumCount(number)}"); // через WriteLine 
+// обращаемся к методу NumCount в которую передаем переменную number
 
 
 
 
-int NumCount(int num)
+int NumCount(int num) // метод принимает int num
 {
-    int digits = 0;
-    while(num > 0)
+    int digits = 0; // вводим отдельную переменную которая пока = 0
+    while(num > 0) // проверяем пока (num > 0)
     {
-        digits++;
-        num /=10;
+        digits++; //увеличиваем эту переменную на 1
+        num /=10; //каждый раз при делении уменьшается на 1 цифру
     }
     return digits;
 }
