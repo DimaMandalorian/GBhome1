5d2ffe1ec31673313f709823e7a96206ace20b20
Dmitrij <accordsds@gmail.com>
2023-01-14 03:30:43 -0800

    На вход число,на выход произведение чисел от 1 до N

---
+++ Seminar2.1/Task24.2/Program.cs
@@ -0,0 +1,26 @@
+﻿// программа принимает на вход число N 
+// на выходе произведение чисел от 1 до N.
+// 4 -> 24   5 -> 120
+
+
+
+using static System.Console;
+Clear();
+
+Write("Input number: " );            
+int.TryParse(ReadLine(), out int number);
+WriteLine($"Произведение ряда чисел от 1 до {number} = {NumProduct(number)}");
+
+
+
+
+int NumProduct(int num)
+{
+    int result = 1;
+    while(num > 0)
+    {
+        result *= num;
+        num--;
+    }
+    return result;
+}
