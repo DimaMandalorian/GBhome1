d4595de6d7fe0f27eb41a60b973de791f63b727c
Dmitrij <accordsds@gmail.com>
2023-01-14 03:33:13 -0800

    Выдает сумму чисел от 1 до N, на которые N делится без остатка

---
+++ Seminar2.1/Task24.3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Программа которая выдает сумму чисел от 1 до N
+// на которые N делится без остатка
+
+using static System.Console;
+Clear();
+
+Write("Input number: " );            
+int.TryParse(ReadLine(), out int number);
+WriteLine($"Произведение ряда чисел от 1 до {number} = {NumProduct(number)}");
+
+
+
+int NumProduct(int num)
+{
+    int result = 0;
+    int temp = num;
+    while(num > 0)
+    {
+        if (temp % num == 0)
+        {
+            result += num;
+        }    
+        num--;
+    }
+    return result;
+}
