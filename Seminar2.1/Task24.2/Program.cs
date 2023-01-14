14a4be569efd4a8051633ed94ce2c2903f8e638c
Dmitrij <accordsds@gmail.com>
2023-01-14 04:03:33 -0800

    Задача на вход число, на выход произведение чисел

--- Seminar2.1/Task24.2/Program.cs
+++ Seminar2.1/Task24.2/Program.cs
@@ -4,23 +4,49 @@
 
 
 
+// using static System.Console;
+// Clear();
+
+// Write("Input number: " );            
+// int.TryParse(ReadLine(), out int number);
+// WriteLine($"Произведение ряда чисел от 1 до {number} = {NumProduct(number)}");
+
+
+
+
+// int NumProduct(int num)
+// {
+//     int result = 1;
+//     while(num > 0)
+//     {
+//         result *= num;
+//         num--;
+//     }
+//     return result;
+// }
+
+// второе решение если число отрицательное
+
 using static System.Console;
 Clear();
 
 Write("Input number: " );            
-int.TryParse(ReadLine(), out int number);
-WriteLine($"Произведение ряда чисел от 1 до {number} = {NumProduct(number)}");
+int n = int.Parse(ReadLine()!);
+
+WriteLine($"product 1..n = {GetMult(n)}");
 
 
 
 
-int NumProduct(int num)
+int GetMult(int A)
 {
+    if(A <= 0) return 0;
+
     int result = 1;
-    while(num > 0)
+    while(A > 0)
     {
-        result *= num;
-        num--;
+        result *= A;
+        A--;
     }
     return result;
 }
