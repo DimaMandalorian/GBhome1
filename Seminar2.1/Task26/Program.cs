07be4b48d238849f2df2ee1e95afd1848ada9090
Dmitrij <accordsds@gmail.com>
2023-01-14 05:55:00 -0800

    Выводим массив из 8 элементов,заполненный 0 и 1 в случаном порядке

---
+++ Seminar2.1/Task26/Program.cs
@@ -0,0 +1,36 @@
+﻿//Программа которая выводит массив из 8 элементов
+//заполненный нулями и единицами в случайном проядке.
+// [1,0,1,1,0,1,0,0]
+
+using static System.Console;
+Clear();
+
+int [] arr = GetArray(8);
+
+PrintArray(arr);
+
+void FullArray (int [] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+    }
+}
+
+int[] GetArray (int size)
+{
+    int [] array = new int [size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(2);
+    }
+    return array;
+}
+
+void PrintArray (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Write($"{array[i]} ");
+    }
+}
