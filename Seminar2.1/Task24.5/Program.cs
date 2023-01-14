976a8d6bcc82dd0377ed2232c21ca5960749bc9e
Dmitrij <accordsds@gmail.com>
2023-01-14 05:09:15 -0800

    Массивы повторение теории

---
+++ Seminar2.1/Task24.5/Program.cs
@@ -0,0 +1,41 @@
+﻿// // Массив
+
+// using static System.Console;
+
+// Clear();
+// Write("Input number: ");
+// int[] ar = IntArray();
+
+
+// for (int i = 0; i < ar.Length; i++)
+// {
+//     ar[i]=int.Parse(ReadLine()!);
+// }
+
+// WriteLine(ar[2]);
+
+// int[] IntBinaryArray()
+// {
+//     int[] res = new int[8];
+//     return res;
+
+// }
+
+// void PrintArray(int[] inArray)
+// {
+
+// }
+
+
+
+//Второй способ
+
+using static System.Console;
+
+Clear();
+int[] ar={12,2,3,4};
+
+for (int i = 0; i < ar.Length; i++)
+{
+    WriteLine(ar[i]);
+}
