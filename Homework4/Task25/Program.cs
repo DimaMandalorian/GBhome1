664c6afcd9f06c2912d683b06e4bd93a5c76aebf
Dmitrij <accordsds@gmail.com>
2023-01-14 23:44:45 -0800

    Возведение числа А в степень В

---
+++ Homework4/Task25/Program.cs
@@ -0,0 +1,20 @@
+﻿// Программа принимает на вход два числа (А и В)
+// и возводит число А в натуральную степень В
+// используем цикл 3,5 ->2 43  2,4 ->16
+
+using static System.Console;
+Clear();
+
+Write("Input A: " ); 
+int A = int.Parse(ReadLine()!);
+Write("Input B: " );
+int B = int.Parse(ReadLine()!);
+
+
+
+int sum = A;
+for (int i = 1; i < B; i++)
+{
+    sum *= A;
+}
+WriteLine($"Результат -> {sum}");
