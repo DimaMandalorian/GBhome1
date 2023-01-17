f9620ee5573c978c6913335851c7eb27536fad20
Dmitrij <accordsds@gmail.com>
2023-01-16 19:51:27 -0800

    Написать метод Возведение числа А в степень В

--- Homework4/Task25/Program.cs
+++ Homework4/Task25/Program.cs
@@ -2,6 +2,8 @@
 // и возводит число А в натуральную степень В
 // используем цикл 3,5 ->2 43  2,4 ->16
 
+
+
 using static System.Console;
 Clear();
 
@@ -9,12 +11,36 @@ Write("Input A: " );
 int A = int.Parse(ReadLine()!);
 Write("Input B: " );
 int B = int.Parse(ReadLine()!);
-
-
-
-int sum = A;
-for (int i = 1; i < B; i++)
-{
-    sum *= A;
-}
+int sum = GetSum(A);
 WriteLine($"Результат -> {sum}");
+
+
+
+int GetSum(int A)
+{
+    int sum = A;
+    for (int i = 1; i < B; i++)
+    {
+        sum *= A;
+    }
+    return sum;
+}
+
+
+
+// using static System.Console;
+// Clear();
+
+// Write("Input A: " ); 
+// int A = int.Parse(ReadLine()!);
+// Write("Input B: " );
+// int B = int.Parse(ReadLine()!);
+
+
+
+// int sum = A;
+// for (int i = 1; i < B; i++)
+// {
+//     sum *= A;
+// }
+// WriteLine($"Результат -> {sum}");
