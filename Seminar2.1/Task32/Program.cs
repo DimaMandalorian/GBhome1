2824090fb28ce3401dc6cc94677346a210c6bba0
Dmitrij <accordsds@gmail.com>
2023-01-18 18:38:28 -0800

    Заменить элементы массива положительные на отрицательные

---
+++ Seminar2.1/Task32/Program.cs
@@ -0,0 +1,29 @@
+﻿//Программа замена элементов массива: положительные элементы 
+// замените на соответствующие отрицательные и наоборот
+// [-4,-8,8,2] -> [4,8,-8,-2]
+
+using static System.Console;
+Clear();
+
+int[] array = { -4, -8, 0, 8, 2};
+
+ReversArray(array);
+PrintArray(array);
+
+void ReversArray(int[] inArray)
+{
+    for (int i = 0; i < inArray.Length; i++)//проходим от нуля до конца длинны массива
+    {
+        inArray[i]*= -1; // перезаписываем значение элемента меняем + на - и наоборот
+    }
+}
+
+void PrintArray(int[] inArray)// метод выводим в консоль 
+{
+    Write("[");
+    for (int i = 0; i < inArray.Length -1; i++)
+    {
+        Write($"{inArray[i]}, ");
+    }
+    WriteLine($"{inArray[inArray.Length - 1]}]");
+}
