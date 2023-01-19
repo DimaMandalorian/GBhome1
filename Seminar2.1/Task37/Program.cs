112cb13b3903f7d67ef1cda8cd9525c157e365a9
Dmitrij <accordsds@gmail.com>
2023-01-18 18:49:19 -0800

    произведение пар чисел в одном массиве

---
+++ Seminar2.1/Task37/Program.cs
@@ -0,0 +1,49 @@
+﻿// Найдите произведение пар чисел в одномерном массиве.
+//Парой считаем первый и последний элемент, второй и предпоследний и тд.
+//Результат запишите в новом массиве.
+
+
+using static System.Console;
+Clear();
+
+int[] array = GetArray(10); // создаем массив 5ти размерным
+PrintArray(array);         // печатаем
+int[] array2 = multiPara(array); // через функцию производим сложение 
+PrintArray(array2);       // снова печатаем
+
+int [] GetArray(int size)
+{
+    int[] resultArray = new int [size];
+
+    Random random = new Random();
+    for (int i = 0; i < size; i++)
+    {
+       resultArray[i] = random.Next(0,10);
+    }
+    return resultArray;
+}
+
+
+int[] multiPara(int[] array)
+{
+    int size = array.Length / 2; //  определяем размер деленное на 2 идем на встречу друг другу
+    if(array.Length % 2 == 1) size++;//если у нас не четное добавляем середину увеличиваем на 1
+    
+    int[] result = new int [size];// создаем новый массив
+    for (int i = 0; i < size; i++)
+    {
+        result [i] = array [i] * array[array.Length -i -1];// наполняем массив 
+    }  // первоt числo array [i] умножаем на число с другой стороны array[array.Length -i -1]
+    return result;// возврящаем результат 
+}
+
+
+void PrintArray(int[] inArray) // inArray название переменной
+{
+    Write("[");
+    for (int i = 0; i < inArray.Length -1; i++)
+    {
+        Write($"{inArray[i]}, ");
+    }
+    WriteLine($"{inArray[inArray.Length-1]}]"); //Что бы последний элемент был без запятой
+}
