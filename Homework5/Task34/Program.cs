ded016f9c681466bd1ba37e6522a3e0cd4ff1612
Dmitrij <accordsds@gmail.com>
2023-01-19 16:34:08 -0800

    Task34

---
+++ Homework5/Task34/Program.cs
@@ -0,0 +1,45 @@
+﻿// Напишите программу реализующую методы формирования массива,
+// заполненного случайными положительными трёхзначными числами, 
+// и подсчета количества чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+using static System.Console;
+Clear();
+
+
+int [] arr = GetArray(4); 
+
+PrintArray(arr);
+
+
+
+int res = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] % 2 == 0) res++;
+}
+WriteLine($" -> {res}");
+
+
+
+int[] GetArray (int size)  
+{
+    int [] array = new int [size];
+    for (int i = 0; i < array.Length; i++) 
+    {
+        array[i] = new Random().Next(100,999); 
+    }
+    return array; 
+}
+
+
+
+void PrintArray(int[] inArray) 
+{
+    Write("[");
+    for (int i = 0; i < inArray.Length -1; i++)
+    {
+        Write($"{inArray[i]}, ");
+    }
+    Write($"{inArray[inArray.Length-1]}]");
+}
