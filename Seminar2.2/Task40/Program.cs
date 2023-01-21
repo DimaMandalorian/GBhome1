5b3deb1c4027419ba570ba446180c7a5bc546594
Dmitrij <accordsds@gmail.com>
2023-01-21 04:00:36 -0800

    На вход 3 числа длины сторон треугольника,существует ли треугольник?

---
+++ Seminar2.2/Task40/Program.cs
@@ -0,0 +1,30 @@
+﻿// программа принимает на вход три числаи проверяет
+// может ли существовать треугольник со сторонами такой длинны
+// существует или не существует
+
+using static System.Console;
+Clear();
+// треугольник
+int a = AskUserForNamber("Введите сторону треугольника a: "); // длинна стороны а
+int b = AskUserForNamber("Введите сторону треугольника b: "); // длинна стороны в
+int c = AskUserForNamber("Введите сторону треугольника c: "); // длинна стороны с
+
+WriteLine(IsTriangle(a,b,c)? "Треугольник существует ":"Треугольник не существует ");
+
+//Переменные сторон
+bool IsTriangle(int inA, int inB, int inC)
+{ // если сумма двух сторон больше третей. Проверяем все три стороны
+    return (inA + inB > inC && inA + inC > inB && inB + inC > inA); // если верно возвращаем да
+}
+
+int AskUserForNamber(string inPromt)
+{
+    int resultNum = 0;
+    string userInput = "";
+    while (!int.TryParse(userInput, out resultNum))
+    {
+        Console.Write(inPromt);
+        userInput = Console.ReadLine()!;
+    }
+    return resultNum;
+}
